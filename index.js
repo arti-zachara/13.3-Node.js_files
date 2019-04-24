d49590d964038bf4d5c58402302abd2c98d81c7e
Magdalena arti Zachara <magdalena.j.zachara@gmail.com>
2019-04-24 20:17:13 +0200

    Add reading and writing functions

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 var fs = require("fs");
 var StatMode = require("stat-mode");
+var colors = require("colors");
 
 fs.stat("./cat.jpg", function(err, stats) {
   var statMode = new StatMode(stats);
   console.log(statMode.toString());
+});
+
+fs.readFile("./tekst.txt", "utf-8", function(err, data) {
+  console.log(data);
+});
+
+fs.readFile("./tekst.txt", "utf-8", function(err, data) {
+  console.log("Dane przed zapisem!".blue);
+  console.log(data);
+  fs.appendFile("./tekst.txt", "\nA tak wyglądają po zapisie!", function(err) {
+    if (err) throw err;
+    console.log("Zapisano!".blue);
+    fs.readFile("./tekst.txt", "utf-8", function(err, data) {
+      console.log("Dane po zapisie".blue);
+      console.log(data);
+    });
+  });
+});
+fs.readdir(".", function saveFile(err, files) {
+  if (err) throw err;
+  console.log("Directory fies have been listed");
+  fs.writeFile("./message.txt", files, function(err) {
+    if (err) throw err;
+    console.log("File has been saved");
+  });
 });
